3dcbc5dfa77f2746c4a9224274de0e6d2f47eb1d
tklqd2502 <tklqd2502@gmail.com>
2019-03-27 20:24:44 -0600

    - Delete unnecessary codes

--- Source/ResidentEvil/Character/Enemy/Zombie/Zombie.cpp
+++ Source/ResidentEvil/Character/Enemy/Zombie/Zombie.cpp
@@ -15,15 +15,14 @@ AZombie::AZombie(const FObjectInitializer& ObjectInitializer) : Super(ObjectInit
 	USkeletalMeshComponent* SkeletalMeshComponent = this->GetMesh();
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SkeletalMeshAsset(TEXT("/Game/MyAssets/AnimStarterPack/UE4_Mannequin/Mesh/SK_Mannequin.SK_Mannequin"));
 	SkeletalMeshComponent->SetSkeletalMesh(SkeletalMeshAsset.Object);
-	SkeletalMeshComponent->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -83.0f), FRotator(0.0f, -90.0f, 0.0f));
 
 	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BehaviorTreeAsset(TEXT("BehaviorTree'/Game/AI/ZombieBT.ZombieBT'"));
 	AIBehaviorTree = BehaviorTreeAsset.Object;
 
 	this->AIControllerClass = AEnemyAIController::StaticClass();
 
-	static ConstructorHelpers::FObjectFinder<UAnimSequence> anim(TEXT("AnimSequence'/Game/MyAssets/AnimStarterPack/Aim_Space_Hip.Aim_Space_Hip'"));
-	Anim = anim.Object;
+	static ConstructorHelpers::FObjectFinder<UAnimSequence> AnimSequenceAsset(TEXT("AnimSequence'/Game/MyAssets/AnimStarterPack/Aim_Space_Hip.Aim_Space_Hip'"));
+	Anim = AnimSequenceAsset.Object;
 }
 
 void AZombie::BeginPlay()
@@ -36,15 +35,4 @@ void AZombie::BeginPlay()
 void AZombie::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	//if (CharacterTarget != nullptr)
-	//{
-	//	FVector ActorLocation = this->GetActorLocation();
-	//	FVector CharacterLocation = CharacterTarget->GetActorLocation();
-	//	FVector Direction = CharacterLocation - ActorLocation;
-	//	Direction.Normalize();
-	//	FRotator RotationDelta = UKismetMathLibrary::FindLookAtRotation(ActorLocation, CharacterLocation);
-	//	FRotator NewRot = FMath::RInterpTo(this->GetActorRotation(), RotationDelta, DeltaTime, 2);
-	//	this->AddMovementInput(Direction, 0.2f, false);
-	//	this->FaceRotation(NewRot);
-	//}
 }
